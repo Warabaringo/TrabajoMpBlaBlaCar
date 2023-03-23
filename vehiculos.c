8a41102d0a9249b1df549098bb37416f4bf70987
pedroceg <101817694+pedroceg@users.noreply.github.com>
2023-03-23 19:18:31 +0100

    CI

--- vehiculos.c
+++ vehiculos.c
@@ -1,19 +1,30 @@
 #include "Vehiculos.h"
 
-//Cabecera:void introducir_vehiculo()
+int main(){
+	
+	Datos_vehiculos* prueba;
+	int num_vehiculos;
+	
+	prueba=(Datos_vehiculos*)malloc(num_vehiculos*sizeof(Datos_vehiculos));
+	
+}
+
+
+//Cabecera:Datos_vehiculo *cargar_estrucura_vehiculo(int *)
 //Condicion:
-//Postcondicion: Se introduce la informacion de un nuevo vehiculo en el fichero "Vehiculos.txt".
-void introducir_vehiculo(){
+//Postcondicion: Carga la estructura en el fichero "Vehiculos.txt".
+Datos_vehiculo *cargar_estrucura_vehiculo(int *numero_vehiculos){
+	
 	char linea_vehiculos[66];
 	FILE *f;
 	Datos_vehiculos *vehiculos;
-	unsigned numero_vehiculos = 0;
+	int numero_vehiculos = 0;
 
 	
 	f=fopen("Vehiculos.txt","w");
 	if(f==NULL) printf("No se ha encontrado el fichero.\n");
 	else{
-		
+		//Falta funcion para meter parametros por teclado creo. ¿aqui o en otra funcion?
 		while(fgets(linea,66,f) != NULL){
 		
 		vehiculos=(Datos_vehiculos *)realloc(vehiculos, numero_vehiculos*sizeof(Datos_vehiculos));
@@ -26,17 +37,44 @@ void introducir_vehiculo(){
 		token = strtok(NULL,"-");
 		strcpy(vehiculos[numero_vehiculos].num_plazas,token);
 		
-		token = strtok(NULL,"-");
+		token = strtok(NULL,"\n");
 		strcpy(vehiculos[numero_vehiculos].descrip_veh,token);
 				
-		numero_vehiculos++;
+		*numero_vehiculos++;
 		
 		}
 		
-		
+		//¿Falta algo aquí?
 	}
 	
 	fclose(f);
+	return vehiculos;
+	free(vehiculos);
+	
+}
+
+
+//Cabecera:void lista_vehiculos()
+//Condicion:
+//Postcondicion: Muestra una lista de los vehiculos que hay en el fichero "Vehiculos.txt".
+void lista_vehiculos(){
+	int i;
+	
+	printf("LISTA VEHICULOS\n\nMatricula-ID Usuario-Numero de plazas-Descripcion\n\n");
+	
+	for(i = 0; i < numero_alumnos; i++){
+	
+		printf("%s-%s-%s-%s\n",Datos_vehiculos[i].matric_veh, Datos_vehiculos[i].id_usu, Datos_vehiculos[i].num_plazas, Datos_vehiculos[i].descrip_veh);
+	
+	}
+	
+	puts(""); //¿Que hacia?
+}
+
+
+
+void introducir_vehiculos{
+	
 	
 	
 }
